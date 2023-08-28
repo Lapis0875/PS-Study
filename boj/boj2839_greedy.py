d21bac40db0f0a5203393eb2364f5c4165b74479
Minjun Kim (Lapis0875) <lapis0875@gmail.com>
2023-08-28 11:01:24 +0000

    feat: Upload codes (2023.08.28)

---
+++ boj/boj2839_greedy.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+N: int = int(stdin.readline())
+
+count: int = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        count += N // 5
+        N = 0
+        break
+    else:
+        N -= 3
+        count += 1
+print(count if N == 0 else -1)
---
+++ boj/boj7568.py
@@ -0,0 +1,13 @@
+from sys import stdin
+from typing import Final
+
+PersonSize = tuple[int, int]        # (키, 몸무게) 쌍의 튜플 타입
+N: Final[int] = int(stdin.readline())
+people: list[PersonSize] = [tuple(map(int, stdin.readline().split())) for _ in range(N)]
+
+for person in people:
+    rank: int = 1
+    for other in people:
+        if person[0] < other[0] and person[1] < other[1]:
+            rank += 1
+    print(rank)
