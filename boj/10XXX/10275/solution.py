03201065122f611c6293e1b5bcf672761df61861
Lapis0875875875 <lapis0875@gmail.com>
2025-08-07 00:10:48 +0900

    feat: Change implmentation of BOJ 10275

--- boj/10XXX/10275/solution.py
+++ boj/10XXX/10275/solution.py
@@ -4,12 +4,14 @@ for _ in range(int(input())):
     if A % 2 == 1: # 홀수 단위로 자르려면 무조건 1까지 나눠야 한다.
         print(N)
     else:
-        magic_cnt = -1
-        total_gold = 1 << N # 2^N
-        while A > 0:
-            if A >= total_gold:
-                A -= total_gold
-            total_gold >>= 1
-            magic_cnt += 1
-        print(magic_cnt)
-    
+        a_n = 1
+        b_n = 1
+        a_cnt = 0
+        b_cnt = 0
+        while a_n & A == 0:
+            a_cnt += 1
+            a_n <<= 1
+        while b_n & B == 0:
+            b_cnt += 1
+            b_n <<= 1
+        print(N - min(a_cnt, b_cnt))
