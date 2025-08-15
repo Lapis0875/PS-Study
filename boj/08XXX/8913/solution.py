d5e1755f25723fef36af5fb33cb5cca19caf80dd
Lapis0875875875 <lapis0875@gmail.com>
2025-08-15 16:59:34 +0900

    feat: Update code (boj 8913)

--- boj/08XXX/8913/solution.py
+++ boj/08XXX/8913/solution.py
@@ -1,51 +1,51 @@
 input = open(0).readline
 
-def get_part_len(part):
+def get_group_len(part):
     return part[1] - part[0]
 
-def dfs(s_info):
+def dfs(string_groups):
     """
-    Backtracking (DFS with condition) using string's partition information.
+    Backtracking (DFS with condition) using string's group information.
     
     Arguments:
-        s_info (list[tuple[int, int]]): list of partition info(tuple of two integers: start & end index)
+        s_info (list[tuple[int, int]]): list of group info(tuple of two integers: start & end index)
     """
-    if len(s_info) == 1: # end of dfs
-        res = s_info[0][1] - s_info[0][0] >= 2 # return whether we can remove last part.
+    if len(string_groups) == 1: # end of dfs
+        res = string_groups[0][1] - string_groups[0][0] >= 2 # return whether we can remove last part.
         return res
     
     res = False
     # Case 1. Remove first elem
-    if get_part_len(s_info[0]) >= 2:
-        res = dfs([*s_info[1:]])
+    if get_group_len(string_groups[0]) >= 2:
+        res = dfs([*string_groups[1:]])
     # Case 2. Remove middle elem
-    for part_idx in range(1, len(s_info) - 1):
-        diff = get_part_len(s_info[part_idx])
+    for gruop_idx in range(1, len(string_groups) - 1):
+        diff = get_group_len(string_groups[gruop_idx])
         if diff < 2:
             continue
-        left = [*s_info[:part_idx - 1]]
-        mid = (s_info[part_idx - 1][0], s_info[part_idx + 1][1] - diff)
+        left = [*string_groups[:gruop_idx - 1]]
+        mid = (string_groups[gruop_idx - 1][0], string_groups[gruop_idx + 1][1] - diff)
         left.append(mid)
-        if part_idx < len(s_info) - 2:
-            left.extend(map(lambda p: (p[0] - diff, p[1] - diff), s_info[part_idx+2:]))
+        if gruop_idx < len(string_groups) - 2:
+            left.extend(map(lambda p: (p[0] - diff, p[1] - diff), string_groups[gruop_idx+2:]))
         res = dfs(left)
         if res:
             break
     # Case 3. Remove last elem
-    if not res and get_part_len(s_info[-1]) >= 2:
-        res = dfs([*s_info[:-1]])
+    if not res and get_group_len(string_groups[-1]) >= 2:
+        res = dfs([*string_groups[:-1]])
     return res
 
 for _ in range(int(input())):
     orig_group = input().strip()
-    parts = []
+    groups = []
     prev = orig_group[0]
     prev_start = 0
     for idx in range(1, len(orig_group)):
         if orig_group[idx] != prev:
-            parts.append((prev_start, idx)) # [start, end)
+            groups.append((prev_start, idx)) # [start, end)
             prev_start = idx
             prev = orig_group[idx]
-    parts.append((prev_start, len(orig_group))) # last part.
-    res = dfs(parts)
+    groups.append((prev_start, len(orig_group))) # last part.
+    res = dfs(groups)
     print("1" if res else "0")
