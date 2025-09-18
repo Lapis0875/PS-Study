26819684135c4a98078b4e2e53bda8ff8e0e8dcb
Lapis0875875875 <lapis0875@gmail.com>
2025-09-18 23:17:00 +0900

    feat: Upload code (25.9.18)

---
+++ boj/02XXX/2418/solution.py
@@ -0,0 +1,34 @@
+input = open(0).readline
+H, W, L = map(int, input().split())
+DIFF = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+letters = [input().rstrip() for _ in range(H)]
+word = input().rstrip()
+
+cnt = 0
+memo = [[[-1 for _ in range(L)] for _ in range(W)] for _ in range(H)]
+def dfs(r, c, depth):
+    print(f"DFS({r},{c}) depth={depth} // TILE = {letters[r][c]}")
+    if depth == L:
+        print(f"=> DEPTH = L")
+        return 1
+    
+    if memo[r][c][depth] != -1:
+        print(f"=> memo: {memo[r][c][depth]}")
+        return memo[r][c][depth]
+    
+    res = 0
+    for dr, dc in DIFF:
+        nr = r + dr
+        nc = c + dc
+        if 0 <= nr < H and 0 <= nc < W and letters[nr][nc] == word[depth]:
+            res += dfs(nr, nc, depth + 1)
+    memo[r][c][depth] = res
+    print(f"=> ({r},{c}) at {word[depth]} = {res}")
+    return res
+
+for r in range(H):
+    for c in range(W):
+        if letters[r][c] == word[0]:
+            cnt += dfs(r, c, 1)
+
+print(cnt)
