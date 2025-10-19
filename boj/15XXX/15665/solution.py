de67614ea5d2ed8c277c03af4f75a060000382f8
Lapis0875875875 <lapis0875@gmail.com>
2025-10-19 16:33:17 +0900

    fix: Fix code of boj 15565: Previous code exceeds time limit.

--- boj/15XXX/15665/solution.py
+++ boj/15XXX/15665/solution.py
@@ -1,8 +1,9 @@
 input = open(0).readline
 N, M = map(int, input().split())
-numbers = [False] * 10001
-for n in map(int, input().split()):
-    numbers[n] = True
+numbers = {}
+for n in sorted(map(int, input().split())):
+    numbers[n] = numbers.get(n, False) or True
+
 arr = [0] * M
 
 def backtracking(depth):
@@ -10,10 +11,9 @@ def backtracking(depth):
         print(" ".join(map(str, arr)))
         return
     
-    for n in range(10001):
-        if numbers[n]:
-            arr[depth] = n
-            backtracking(depth + 1)
+    for n in numbers:
+        arr[depth] = n
+        backtracking(depth + 1)
     arr[depth] = ""
 
 backtracking(0)
