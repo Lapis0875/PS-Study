c8dbd99ca73f9abae2a3a626f3f39de6084f87b7
Lapis0875 <lapis0875@kakao.com>
2026-02-17 12:35:21 +0900

    feat: Update Python code template.
    - Apply function structure for python optimization.
    - Apply Fast Input using io.BytesIO, but we should consider that CPH Judge plugin does not work with this way.

--- boj/templates/solution.py
+++ boj/templates/solution.py
@@ -1,1 +1,7 @@
-input = open(0).readline
+import io, os
+
+def main():
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline # 빠른 입출력. 하지만 CPH Judge에서 돌아가지 않음에 유의할 것.
+    # input = open(0).readline # CPH Judge에서 결과 보려면 일단 이거로 바꾸고 써보자
+
+main()
